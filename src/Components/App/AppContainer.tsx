d169da1ae7aaa264a5d195c6949682d800960801
jungpaeng <meis1541@naver.com>
2019-03-15 23:29:29 +0900

    :sparkles: Connect Master Node at onLoad

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,6 +1,8 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import { MASTER_NODE, SELF_NODE, SELF_P2P_NODE } from '../../Constants/server';
 import typography from '../../typography';
 import AppPresenter from './AppPresenter';
 
@@ -10,6 +12,16 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 class AppContainer extends Component {
+  public componentDidMount() {
+    this.connectOnMaster();
+  }
+
+  public connectOnMaster = async () => {
+    const request = await axios.post(`${MASTER_NODE}/peers`, {
+      peer: SELF_P2P_NODE,
+    });
+  }
+
   public render() {
     return (
       <>
--- src/Constants/server.ts
+++ src/Constants/server.ts
@@ -2,4 +2,4 @@ import '../@types/global.d';
 
 export const MASTER_NODE = 'https://js-bitcoin.herokuapp.com';
 export const SELF_NODE = `http://localhost:${window.sharedPort}`;
-export const SELF_P2P_NODE = port => `ws://localhost:${port}`;
+export const SELF_P2P_NODE = `ws://localhost:${window.sharedPort}`;
