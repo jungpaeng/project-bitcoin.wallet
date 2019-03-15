5a48bf13f113cf27b9c8240ab903c3c9b908fdfa
jungpaeng <meis1541@naver.com>
2019-03-15 23:47:33 +0900

    :sparkles: Get Balance from Local Node

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -17,17 +17,21 @@ interface IProps {
 
 interface IState {
   address: string;
+  balance: number;
 }
 
 class AppContainer extends Component<IProps, IState> {
   public state: IState = {
     address: '',
+    balance: 0,
   };
 
   public componentDidMount() {
     const { sharedPort } = this.props;
     this.connectOnMaster(sharedPort);
     this.getAddress(sharedPort);
+    this.getBalance(sharedPort);
+    setInterval(() => this.getBalance(sharedPort), 1000);
   }
 
   public connectOnMaster = async (port: string) => {
@@ -40,6 +44,12 @@ class AppContainer extends Component<IProps, IState> {
     const { data: { address } } = await axios.get(`${SELF_NODE(port)}/me/address`);
 
     this.setState({ address });
+  }
+
+  public getBalance = async (port: string) => {
+    const { data: { balance } } = await axios.get(`${SELF_NODE(port)}/me/balance`);
+
+    this.setState({ balance });
   }
 
   public render() {
